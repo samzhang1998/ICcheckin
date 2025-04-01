643d8cace3f269b75551abf37d58a6a6aac0c180
hongyuan dan <hongyuandan@hongyuandeMacBook-Pro.local>
2025-04-01 16:57:02 +1100

    Add detailed logging to API requests for debugging

--- src/api/base.js
+++ src/api/base.js
@@ -2,6 +2,9 @@ const baseUrl = "http://13.211.159.140";
 
 function request (url, method, data = {}) {
     return new Promise((resolve, reject) => {
+        console.log(`Making ${method} request to ${baseUrl}${url}`, data);
+        console.log("Using token:", uni.getStorageSync("token"));
+        
         uni.request({
             url: `${baseUrl}${url}`,
             method,
@@ -10,9 +13,12 @@ function request (url, method, data = {}) {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${uni.getStorageSync("token")}`
             },
-            success: (res) => resolve(res),
+            success: (res) => {
+                console.log(`Response from ${url}:`, res);
+                resolve(res);
+            },
             fail: (err) => {
-                console.error("API Request Error:", err);
+                console.error(`API Request Error for ${url}:`, err);
                 reject(err);
             }
         });
